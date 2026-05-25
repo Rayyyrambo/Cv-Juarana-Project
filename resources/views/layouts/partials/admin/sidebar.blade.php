5c252081306d6f42be8d1ff346d933cff9d00b13
Muh Rayyan Maulana <rayyanmaulana2004@gmail.com>
2026-05-26 06:23:22 +0800

    menambahkan group categories 26 mei 2026

--- resources/views/layouts/partials/admin/sidebar.blade.php
+++ resources/views/layouts/partials/admin/sidebar.blade.php
@@ -11,7 +11,7 @@
 
             <!-- Nav Item - Dashboard -->
            <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="{{ route('admin.dashboard') }}">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
@@ -32,7 +32,7 @@
             </li>
 
             <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="{{ route('admin.categories.index') }}">
                     <i class="fas fa-fw fa-chart-area"></i>
                     <span>Kategori</span></a>
             </li>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\Admin\DasboardController;
+use App\Http\Controllers\Admin\CategoriController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,9 +13,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/service',[ServiceController::class, 'index'])->name('service');
 Route::get('/product', [ProductController::class, 'index'])->name('product');
 Route::get('/project',[ProjectController::class,  'index'])->name('project');
-Route::get('/dasboard',[DasboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
+Route::prefix('administrator')->middleware(['auth', 'verified'])->name('admin.')->group(function(){
+    Route::get('dashboard',[DasboardController::class, 'index'])->name('dashboard');
+    Route::resource('categories', CategoriController::class);
+});
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
